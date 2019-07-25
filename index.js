8d6367ddfa4926527cb690eb35caa731a88999c0
nour-alhuda <nouralasali@hotmail.com>
2019-07-25 14:46:24 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 /* Your code goes here */
+
+  var customerName = 'bob';
+  
+  function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+  }
+  
+  function setBestCustomer(){
+    bestCustomer = 'not bob';
+  }
+  
+  function overWriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+  }
+  
+  const leastFavoriteCustomer = 'Mona';
+  
+  function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Sami';
+  } 
+  let favoriteCustomer = 'Amer';
+  function attemptTwoFavoriteCustomers() {
+    let favoriteCustomer ='Ali';
+    throw ('SyntaxError');
+  }
+  
